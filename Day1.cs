1ba55b71953549f95ebc1bfca04866a00daf73f9
Karol Hartwig <43039961+xGuti@users.noreply.github.com>
2023-12-03 10:53:11 +0100

    Dodaj pliki projektów.

---
+++ Day1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+	internal class Day1
+	{
+		static public void Solve()
+		{
+			String line;
+			try
+			{
+				StreamReader sr = new StreamReader("input1.txt");
+				do
+				{
+					line = sr.ReadLine();
+					Console.WriteLine(line);
+				} while (sr != null);
+			}catch (Exception ex) { Console.WriteLine(ex.Message); }
+		}
+		
+	}
+}
